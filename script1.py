561fea680388c2dc7de006d5cfa19b409b62072a
SreehariJiji <sjiji@flemingcollege.ca>
2024-01-18 01:09:19 -0500

    delete and added line in other computer
    deleted print in script1 and added one more line in script2

--- script1.py
+++ script1.py
@@ -1,4 +1,4 @@
 # no code
 print("Special cases aren't special enough to break the rules.")
-print("Although practicality beats purity.")
+
 #modified
--- script2.py
+++ script2.py
@@ -2,4 +2,5 @@
 print("Beautiful is better than ugly")
 print("Explicit is better than implicit.")
 print("Errors should never pass silently.")
+print("If the implementation is easy to explain, it may be a good idea.")
 #modified
